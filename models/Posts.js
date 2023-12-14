e1d1aaf6920a9af75f64ce62bdc5f8633d8951fb
ellysaschottler <ellysastork@gmail.com>
2023-12-14 09:52:59 -0600

    updated the homepage to be the blog content with title page, updated the blog listing page to be just titles

---
+++ models/Posts.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Posts extends Model {}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_content: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    post_date: {
+        type: DataTypes.DATE,
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'posts',
+  }
+);
+
+module.exports = Posts;
