af1a09fbe584ef78c62e034e58a0d04d050a1492
ellysaschottler <ellysastork@gmail.com>
2024-01-05 11:52:17 -0600

    added seed data

--- seed.js
+++ seed.js
@@ -1,17 +1,35 @@
-const sequelize = require(".config/connections")
-const Posts = require("./models/Posts");
+// const sequelize = require(".config/connections")
+// const Posts = require("./models/Posts");
 
-const postSeedData = require("./post_seed.json")
+// const postSeedData = require("./post_seed.json")
+
+// const seedDatabase = async () => {
+//     await sequelize.sync({ force: true})
+
+//     const post = await Posts.bulkCreate(postSeedData, {
+//         individualHooks: true,
+//         returning: true
+//     })
+// process.exit(0)
+
+// }
+
+// seedDatabase();
+
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+
+const userData = require('./userData.json');
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true})
+  await sequelize.sync({ force: true });
 
-    const post = await Posts.bulkCreate(postSeedData, {
-        individualHooks: true,
-        returning: true
-    })
-process.exit(0)
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-}
+  process.exit(0);
+};
 
 seedDatabase();
