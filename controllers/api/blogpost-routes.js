ff50a378b6dbb70a18b0a7dcaa541a788f146b0b
ellysaschottler <ellysastork@gmail.com>
2024-01-12 10:40:32 -0600

    updated the post blog route

--- controllers/api/blogpost-routes.js
+++ controllers/api/blogpost-routes.js
@@ -1,18 +1,15 @@
-// to do - update dish attributes to blog post attributes
-
 const router = require('express').Router();
 const Posts = require('../../models/Posts');
+const withAuth = require('../../utils/authorization')
 
-// route to create/add a blog post using async/await
-router.post('/', async (req, res) => {
+// route to create/add a blog post using async/await - takes in the data from the new_post handlebar's form
+router.post('/', withAuth, async (req, res) => {
   try { 
     const postData = await Posts.create({
-    dish_name: req.body.dish_name,
-    description: req.body.description,
-    guest_name: req.body.guest_name,
-    has_nuts: req.body.has_nuts,
+        user_id: req.session.user_id,
+        post_title: req.body.dish_name,
+        post_content: req.body.description,
   });
-  // if the blog post is successfully created, the new response will be returned as json
   res.status(200).json(postData)
 } catch (err) {
   res.status(400).json(err);
